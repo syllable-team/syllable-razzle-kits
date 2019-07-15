d05fa8114819d8dc169811911f69d8f889cc37db
Eric Chaves <erchaves@gmail.com>
2019-07-15 17:32:42 -0400

    updating to new razzle, adding heroku and scss, clean up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
-import Home from './Home';
-import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import Comp404 from './Components/404';
+import './App.scss';
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path='/about' component={About} />
+    <Route component={Comp404} />
   </Switch>
 );
 
---
+++ src/Components/About/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.scss';
+
+function About() {
+  return (
+    <div className="page page-about">
+      <h1 className="h1">
+        About
+      </h1>
+    </div>
+  );
+}
+
+
+export default About;
